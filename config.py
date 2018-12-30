6b1cec994bc36bc2b9e27ea9c5aeab91ca058714
MaciekTalaska <maciek.talaska@gmail.com>
2018-12-30 05:18:28 +0100

    automatically restore recently used connection

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
 import pickle
 
 configFileName = '__patricia-connection.dat'
+import os
 
 class PatriciaConfig:
     @staticmethod
+    def configExists():
+        return os.path.isfile(configFileName)
+
+    @staticmethod
     def read():
-        pass
+        data = dict()
+        exists = PatriciaConfig.configExists()
+        if exists:
+            infile = open(configFileName, 'rb')
+            data = pickle.load(infile)
+            infile.close()
+        return data
 
     @staticmethod
     def save(data):
--- psql_main.py
+++ psql_main.py
@@ -6,40 +6,52 @@ from PyQt5.QtWidgets import *
 from patriciasql_main import Ui_MainWindow
 #from db_settings import Ui_dbSettingsDialog
 from db_settings_logic import DBSettingsDialog
+from config import PatriciaConfig
 
 import sys
 import db
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-      def __init__(self, *args, **kwargs):
-            super(MainWindow, self).__init__(*args, **kwargs)
-            self.setupUi(self)
-            self.show()
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.show()
+        # wire up signals & slots
+        self.actionQuit.triggered.connect(self.exitApplication)
+        self.actionExecute.triggered.connect(self.executeQuery)
+        self.actionSettings.triggered.connect(self.showSettings)
+        config = PatriciaConfig.read()
+        self.__connect2Database__(config)
 
-            # wire up signals & slots
-            self.actionQuit.triggered.connect(self.exitApplication)
-            self.actionExecute.triggered.connect(self.executeQuery)
-            self.actionSettings.triggered.connect(self.showSettings)
+    def exitApplication(self):
+		sys.exit(0)
 
-      def exitApplication(self):
-			sys.exit(0)
+    def __connect2Database__(self, connProps):
+        if (len(connProps))>=4:
+            self.db = db.PostgreSQL(userName=connProps['user'], hostName=connProps['host'], password=connProps['password'], databaseName=connProps['db'])
+        else:
+            self.db = None
 
-      def showSettings(self):
-            dialog = DBSettingsDialog()
-            retval = dialog.exec_()
-            if retval == 1:
-                  connProps = dialog.getData()
-                  self.db = db.PostgreSQL(userName=connProps['user'], hostName=connProps['host'], password=connProps['password'], databaseName=connProps['db'])
+    def showSettings(self):
+         dialog = DBSettingsDialog()
+         retval = dialog.exec_()
+         if retval == 1:
+             connProps = dialog.getData()
+             self.__connect2Database__(connProps)
 
-      def executeQuery(self):
+    def executeQuery(self):
+        if self.db is not None:
             queryText = self.sqlEditorArea.toPlainText()
             model = self.db.getModel(queryText)
             self.tableView.setModel(model)
             self.tableView.show()
-
+        else:
+            msg = QMessageBox()
+            msg.setText("Not connected to DB!")
+            msg.exec_()
 
 if __name__ == "__main__":
-      app = QApplication([])
-      app.setApplicationName("PatriciaSQL")
-      window = MainWindow()
-      app.exec_()
+    app = QApplication([])
+    app.setApplicationName("PatriciaSQL")
+    window = MainWindow()
+    app.exec_()
