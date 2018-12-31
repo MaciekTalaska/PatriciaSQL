a8ce8903693c635e4345dee2961e5afd9d7fd736
MaciekTalaska <maciek.talaska@gmail.com>
2018-12-31 21:30:46 +0100

    it is almost 2019, so Python3 should be default

--- psql_main.py
+++ psql_main.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-
 from PyQt5 import uic
-
 Ui_MainWindow, _ = uic.loadUiType("patriciasql_main.ui")
 from db_settings_logic import DBSettingsDialog
 from config import PatriciaConfig
-
 import sys
 import db
 
@@ -21,12 +18,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.actionQuit.triggered.connect(self.exitApplication)
         self.actionExecute.triggered.connect(self.executeQuery)
         self.actionSettings.triggered.connect(self.showSettings)
+        # read config & use last saved connection
         patriciaConfig = PatriciaConfig()
         config = patriciaConfig.getConfig()
         self.updateDBConnection(config)
 
     def exitApplication(self):
-		sys.exit(0)
+        sys.exit(0)
 
     def updateDBConnection(self, conp):
         self.db = db.PostgreSQL(conp)
@@ -36,11 +34,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.lbldb.setText("connected to: none")
 
     def showSettings(self):
-         dialog = DBSettingsDialog()
-         retval = dialog.exec_()
-         if retval == 1:
-             conp = dialog.getData()
-             self.updateDBConnection(conp)
+        dialog = DBSettingsDialog()
+        retval = dialog.exec_()
+        if retval == 1:
+            conp = dialog.getData()
+            self.updateDBConnection(conp)
 
     def executeQuery(self):
         if self.db is not None:
