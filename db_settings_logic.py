8423d416c577689841502df580d5edcb71c53c22
MaciekTalaska <maciek.talaska@gmail.com>
2019-01-02 05:56:25 +0100

    connection management improved

--- db_settings_logic.py
+++ db_settings_logic.py
@@ -5,37 +5,42 @@ from db import PostgreSQL
 from config import PatriciaConfig
 
 class DBSettingsDialog(QtWidgets.QDialog, ui_dialog):
-    def __init__(self):
+    def __init__(self, pgsql):
         QtWidgets.QDialog.__init__(self)
         ui_dialog.__init__(self)
         self.setupUi(self)
         self.btnResetPort.clicked.connect(self.resetPort)
-        self.btnTestConnection.clicked.connect(self.testConnection)
-        self.btnBrowseDatabases.clicked.connect(self.populateAvailableDBs)
+        self.btnTestConnection.clicked.connect(self.__testConnection__)
+        self.btnBrowseDatabases.clicked.connect(self.__populateAvailableDBs__)
+        self.pgsql = pgsql
 
     def resetPort(self):
         self.txtPort.setText('5432')
 
-    def populateAvailableDBs(self):
-        connProps = self.createConnectionProperties()
-        dbs = PostgreSQL.retrieveAvailableDatabases(connProps)
+    def __populateAvailableDBs__(self):
+        conp = self.__createConnectionProperties__()
+        dbs = self.pgsql.retrieveAvailableDatabases(conp)
         for i in range(len(dbs)):
             self.cbxDBs.addItem(str(dbs[i]))
+#        connProps = self.__createConnectionProperties__()
+#        dbs = PostgreSQL.retrieveAvailableDatabases(connProps)
+#        for i in range(len(dbs)):
+#            self.cbxDBs.addItem(str(dbs[i]))
 
-
-    def createConnectionProperties(self):
+    def __createConnectionProperties__(self):
         cp = dict()
         cp['host'] = self.txtHostName.text()
         cp['port'] = self.txtPort.text()
         cp['user'] = self.txtUserName.text()
         cp['password'] = self.txtPassword.text()
         cp['db'] = self.cbxDBs.currentText()
+        #cp['db'] = self.cbxDBs.text()
         return cp
 
-    def testConnection(self):
-        connProps = self.createConnectionProperties()
+    def __testConnection__(self):
+        connProps = self.__createConnectionProperties__()
         msg = QtWidgets.QMessageBox()
-        if PostgreSQL.checkConnection(connProps):
+        if self.pgsql.checkConnection(connProps):
             msg.setText("Success!")
             msg.exec_()
         else:
@@ -43,7 +48,26 @@ class DBSettingsDialog(QtWidgets.QDialog, ui_dialog):
             msg.exec_()
 
 
-    def getData(self):
-        props = self.createConnectionProperties()
+#        connProps = self.__createConnectionProperties__()
+#        msg = QtWidgets.QMessageBox()
+#        if PostgreSQL.checkConnection(connProps):
+#            msg.setText("Success!")
+#            msg.exec_()
+#        else:
+#            msg.setText("Error!")
+#            msg.exec_()
+
+    def getConnectionProperties(self):
+        props = self.__createConnectionProperties__()
         PatriciaConfig.save(props)
-        return self.createConnectionProperties()
+        return self.__createConnectionProperties__()
+
+    # Ideally this should be used to display Dialog and get connection properties from it
+    # It may be a good idea to pass old (current) connection as parameter, so in case 'Cancel' is clicked, it is returned
+    # OR
+    # return false (cancel) or true, depending on what button hsa been clicked, so that parent window does not reconnect to db if it is not needed
+#    @staticmethod
+#    def getConnectionProperties():
+#        dialog = DBSettingsDialog()
+#        retval = dialog.exec_()
+#        return dialog.getConnectionProperties()
--- psql_main.py
+++ psql_main.py
@@ -3,9 +3,11 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+
 Ui_MainWindow, _ = uic.loadUiType("patriciasql_main.ui")
 from db_settings_logic import DBSettingsDialog
 from config import PatriciaConfig
+
 import sys
 import db
 
@@ -18,32 +20,33 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.actionQuit.triggered.connect(self.exitApplication)
         self.actionExecute.triggered.connect(self.executeQuery)
         self.actionSettings.triggered.connect(self.showSettings)
-        # read config & use last saved connection
+        # read config
         patriciaConfig = PatriciaConfig()
         config = patriciaConfig.connectionConfig
+        # try to connect (most recent connection)
+        self.pgsql = db.PostgreSQL()
         self.updateDBConnection(config)
 
     def exitApplication(self):
         sys.exit(0)
 
     def updateDBConnection(self, conp):
-        self.db = db.PostgreSQL(conp)
-        if self.db is not None:
-            self.lbldb.setText("connected to: " + conp['db'])
-        else:
-            self.lbldb.setText("connected to: none")
+        self.pgsql.reconnect(conp)
+        # TODO change it, so dbname is retrieved from pgsql
+        self.lbldb.setText("connected to: " + str(conp.get('db')))
+        #self.lbldb.setText("connected to: " + self.pgsql.getCurrentDBName())
 
     def showSettings(self):
-        dialog = DBSettingsDialog()
+        dialog = DBSettingsDialog(self.pgsql)
         retval = dialog.exec_()
         if retval == 1:
-            conp = dialog.getData()
+            conp = dialog.getConnectionProperties()
             self.updateDBConnection(conp)
 
     def executeQuery(self):
-        if self.db is not None:
+        if self.pgsql is not None:
             queryText = self.sqlEditorArea.toPlainText()
-            model = self.db.getModel(queryText)
+            model = self.pgsql.getModel(queryText)
             self.tableView.setModel(model)
             self.tableView.show()
             self.lblstatus.setText("rows: " + str(model.rowCount()))
@@ -53,7 +56,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             msg.exec_()
             self.lblstatus.setText("rows: " + str(model.rowCount()))
 
+
+def trace_calls(frame, event, arg):
+    print('--------')
+    print('frame: ' + str(frame))
+    print('  func name: ' + frame.f_code.co_name)
+    print('  line: ' + str(frame.f_lineno))
+    print('  filename: ' + frame.f_code.co_filename)
+    print('event: ' + event)
+    print('arg:   ' + str(arg))
+    print('--------')
+    if event != 'exception':
+        return
+
 if __name__ == "__main__":
+    #sys.settrace(trace_calls)
     app = QApplication(sys.argv)
     app.setApplicationName("PatriciaSQL")
     window = MainWindow()
