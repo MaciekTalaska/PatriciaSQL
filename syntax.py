5c81fa629671e22876103aded2f5f67716013adc
MaciekTalaska <maciek.talaska@gmail.com>
2019-01-06 20:44:37 +0100

    read syntax hl definitios from external file

--- syntax.py
+++ syntax.py
@@ -1,70 +1,22 @@
 from PyQt5.QtCore import QRegExp
 from PyQt5.QtGui import QColor, QTextCharFormat, QSyntaxHighlighter
 
-hldata = \
-    [
-        {
-            'name': 'keywords',
-            'color': '0x0087ff',
-            'words': ['all', 'analyse', 'analyze', 'array', 'as', 'asc', 'authorization', 'both', 'case', 'cast',
-                      'check', 'collate', 'column', 'concurrently', 'constraint', 'create', 'current_catalog',
-                      'current_date', 'current_role', 'current_schema', 'current_time', 'current_timestamp',
-                      'current_user', 'defaults', 'deferrable', 'desc', 'distinct', 'do', 'else', 'end', 'except',
-                      'false', 'fetch', 'for', 'foreign', 'from', 'full', 'grant', 'group by', 'having', 'ilike',
-                      'initially', 'intersect', 'into', 'lateral', 'limit', 'localtime', 'localtimestamp', 'null',
-                      'offset', 'on', 'only', 'order by', 'placing', 'primary', 'references', 'returning', 'select',
-                      'session_user', 'similar', 'some', 'symmetric', 'table', 'then', 'trailing', 'true', 'union',
-                      'unique', 'user', 'variadic', 'version', 'when', 'window', 'with'],
-            'regex': '',
-        },
-        {
-            'name': 'operators',
-            'color': '0xdc322f',
-            'words': ['\+', '-', '\*', '/', '\%', '\&', '\|', '\^', '=', '>', '<', '>=', '<=', '<>', '\+=', '-=', '\*=',
-                      '/=', '\%=', '\%=', '\^-=', '\|*=', ' all ', ' and ', ' any ', ' between ', ' exists ', ' in ',
-                      ' like ', ' not ', ' or ', ' some ', ],
-            'regex': ''
-        },
-        {
-            'name': 'strings',
-            'color': '0xd33682',
-            'words': [],
-            'regex': r'"[^"\\]*(\\.[^"\\]*)*"'
-        },
-        {
-            'name': 'strings',
-            'color': '0xd33682',
-            'words': [],
-            'regex': r'"[^"\\]*(\\.[^"\\]*)*"'
-        },
-        {
-            'name': 'numbers',
-            'color': '0x268bd',
-            'words': [],
-            'regex': r'[+-]?\b\d+\b'
-        },
-        {
-            'name': 'comments',
-            'color': '0x839496',
-            'words': [],
-            'regex': r'--.*$'
-        },
-    ]
-
 
 class PgSQLHighlighter(QSyntaxHighlighter):
 
     def __init__(self, document):
         QSyntaxHighlighter.__init__(self, document)
 
-        rules = self.process_data(hldata)
+        raw_data = open('colors.txt', 'rb').read()
+        highlight_data = eval(raw_data)
+        rules = self.process_data(highlight_data)
 
         self.rules = [(QRegExp(pattern), color)
                       for (pattern, color) in rules]
 
-    def process_data(self, highlightdata):
+    def process_data(self, highlight_defs):
         rules = []
-        for g in highlightdata:
+        for g in highlight_defs:
             # color
             text_color = g.get('color')
             color = QColor.fromRgb(int(text_color, 16))
