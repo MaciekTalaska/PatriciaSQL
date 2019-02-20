42721ba9ebb6aff147ffda6eb67b3758f61ec7f6
MaciekTalaska <maciek.talaska@gmail.com>
2019-02-20 14:22:17 +0100

    minor code improvements

--- csv_exporter.py
+++ csv_exporter.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
 from PyQt5 import QtCore
 from PyQt5 import QtSql
 
@@ -8,18 +8,20 @@ CSV_FILE_FILTER = "CSV files (*.csv)"
 class CSV:
     @staticmethod
     def write_csv(model: QtSql.QSqlQueryModel):
-        filename, _ = QtWidgets.QFileDialog.getSaveFileName(None, "Export data to CSV",
-                                                            ".",
-                                                            CSV_FILE_FILTER, CSV_FILE_FILTER)
+        filename, _ = QFileDialog \
+            .getSaveFileName(None, "Export data to CSV",
+                             ".",
+                             CSV_FILE_FILTER, CSV_FILE_FILTER)
         data = list()
         with open(filename, "a") as f:
+            # process headers
             for i in range(0, model.columnCount()):
                 header = str(model.headerData(i, QtCore.Qt.Horizontal, QtCore.Qt.DisplayRole))
                 data.append('"' + header + '"')
-
+            # write headers
             f.write(";".join(data))
             f.write("\n")
-
+            # process rows
             for i in range(model.rowCount()):
                 data.clear()
                 for j in range(model.columnCount()):
