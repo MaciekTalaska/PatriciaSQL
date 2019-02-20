7573b3465038966ff0dc342499b349d5fea136f5
MaciekTalaska <maciek.talaska@gmail.com>
2019-02-20 12:20:12 +0100

    code reformat

--- sql_file.py
+++ sql_file.py
@@ -1,21 +1,24 @@
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
 
 SQL_FILE_FILTER = "SQL files (*.sql)"
+
 
 class SQLFile:
     @staticmethod
     def save(content: str):
-        filename, _ = QtWidgets.QFileDialog.getSaveFileName(None, "Export data to CSV",
-                                                            ".",
-                                                            SQL_FILE_FILTER, SQL_FILE_FILTER)
+        filename, _ = QFileDialog \
+            .getSaveFileName(None, "Export data to CSV",
+                             ".",
+                             SQL_FILE_FILTER, SQL_FILE_FILTER)
         with open(filename, "a") as f:
             f.writelines(content)
 
     @staticmethod
     def load():
-        filename, _ = QtWidgets.QFileDialog.getOpenFileName(None, "Export data to CSV",
-                                                            ".",
-                                                            SQL_FILE_FILTER, SQL_FILE_FILTER)
+        filename, _ = QFileDialog \
+            .getOpenFileName(None, "Export data to CSV",
+                             ".",
+                             SQL_FILE_FILTER, SQL_FILE_FILTER)
         with open(filename, "r") as f:
             data = f.readlines()
         return "".join(data)
